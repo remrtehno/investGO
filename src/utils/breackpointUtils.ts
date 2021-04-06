c6e2d90d8f26e4a74e24cf333d4d1a88eabce06e
Андрей Бобриков <Andrey Bobrikov>
2021-04-06 14:38:31 +0300

    Fix adaptive breackpoints

--- src/utils/breackpointUtils.ts
+++ src/utils/breackpointUtils.ts
@@ -1,17 +1,23 @@
+import _ from 'lodash';
+
 import type {adaptiveBreackpoints} from 'src/contstants/adaptiveBreackpoints';
-import {useViewportSize} from 'src/hooks/useViewportSize';
+import {getViewportSize} from 'src/utils/getViewportSize';
+
+const memoViewportSize = _.memoize(() => {
+  return getViewportSize();
+});
 
 export function breackpointUp(breackpoint: adaptiveBreackpoints): boolean {
-  const viewportSize = useViewportSize();
+  const viewportSize = memoViewportSize();
   return viewportSize.width >= breackpoint;
 }
 
 export function breackpointDown(breackpoint: adaptiveBreackpoints): boolean {
-  const viewportSize = useViewportSize();
+  const viewportSize = memoViewportSize();
   return viewportSize.width < breackpoint;
 }
 
 export function breackpointBetween(lower: adaptiveBreackpoints, upper: adaptiveBreackpoints): boolean {
-  const viewportSize = useViewportSize();
+  const viewportSize = memoViewportSize();
   return viewportSize.width >= lower && viewportSize.width < upper;
 }
