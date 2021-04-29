27690699a29b3f1432e79a652ece7ce27d704ca8
Андрей Бобриков <Andrey Bobrikov>
2021-04-29 13:03:06 +0300

    loan request details page

--- src/components/pages/CreateLoanRequestPage/CreateLoanRequestPage.tsx
+++ src/components/pages/CreateLoanRequestPage/CreateLoanRequestPage.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import {RoutePaths} from 'src/components/common/App/routes';
 import {Page} from 'src/components/common/Page';
+import {PageTitle} from 'src/components/common/Page/PageTitle';
 import {TopMenu} from 'src/components/common/TopMenu';
 import {withAuth} from 'src/components/hocs/withAuth';
 import {Text, TextSize} from 'src/components/ui/Text';
 
-import s from './LoanRequestPage.scss';
+import s from './CreateLoanRequestPage.scss';
+import {LoanRequestForm} from './LoanRequestForm';
 
-export declare namespace LoanRequestPage {
+export declare namespace CreateLoanRequestPage {
 }
 
 const menuItems = () => {
@@ -30,14 +32,16 @@ const menuItems = () => {
   );
 };
 
-export const LoanRequestPage = withAuth(() => {
+export const CreateLoanRequestPage = withAuth(() => {
   return (
     <Page>
       <div className={s.loanRequestPage}>
         <TopMenu items={menuItems()} />
+        <PageTitle>Новая заявка на привлечение инвестиций</PageTitle>
         <div className='container'>
           <div className={s.content}>
-            <Text size={TextSize.h3} className={s.header}>Заявка #</Text>
+            <Text size={TextSize.h3} className={s.header}>Параметры займа</Text>
+            <LoanRequestForm />
           </div>
         </div>
       </div>
